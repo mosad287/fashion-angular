db840ca937e78ba39559829c9973e071fc585659
mosad287 <mosadalbarbary832@gmail.com>
2025-12-24 15:22:48 +0200

    fix auth navbar dropdown isho

--- src/app/components/navbar/authnavbar/authnavbar.component.ts
+++ src/app/components/navbar/authnavbar/authnavbar.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { LangdropdownComponent } from '../../../shared/langdropdown/langdropdown.component';
 import { RouterLink } from '@angular/router';
+import { TranslationService } from '../../../core/services/translation.service';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-authnavbar',
@@ -10,4 +12,15 @@ import { RouterLink } from '@angular/router';
   templateUrl: './authnavbar.component.html',
   styleUrl: './authnavbar.component.css',
 })
-export class AuthnavbarComponent {}
+export class AuthnavbarComponent implements OnInit {
+  readonly _PLATFORM_ID = inject(PLATFORM_ID);
+  readonly _TranslationService = inject(TranslationService);
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this._PLATFORM_ID)) {
+      if (localStorage.getItem('lang') === 'ar') {
+        this._TranslationService.checkLang.set(true);
+      }
+    }
+  }
+}
